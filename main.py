a917b318cc7b03577ba83e7f2ef19d07cc72cabc
dead-shadow-7 <deadshadow12119@gmail.com>
2026-09-15 16:10:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+from fastapi import FastAPI
+import inngest 
+import inngest.fast_api
+from inngest.experimental import ai
+
+from dotenv import load_dotenv
+import uuid 
+import os
+import datetime
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id = "rag_app",
+    logger = logging.getLogger("uvicorn"),
+    is_production = False,
+    serializer = inngest.PydanticSerializer()
+)
+
+app = FastAPI()
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+inngest.fast_api.serve(app, inngest_client,[])
